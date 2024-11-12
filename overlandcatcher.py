c24bd1dafa6d06691e64f34a14ead7c7bbcab554
Doug <doug@dougfredericks.net>
2024-11-11 20:15:34 -0800

    successful post

--- overlandcatcher.py
+++ overlandcatcher.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/json", methods=["POST"])
+def json():
+    if request.is_json:
+        req = request.get_json()
+        print(type(req))
+        print(req)
+        return "JSON recd!", 200
+    else:
+        return "No JSON recd", 400
+
