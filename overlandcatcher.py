f39d1b40ef9f173b92df77e16b9d4c0c4ee2a570
Doug <doug@dougfredericks.net>
2024-11-13 19:41:17 -0800

    par

--- overlandcatcher.py
+++ overlandcatcher.py
@@ -10,13 +10,41 @@ def hello_world():
 def json():
     if request.is_json:
         req = request.get_json()
-        response = {
-                "message": "JSON recd",
-                "name": req.get("name")
+        incoming = {
+            "locations": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            -122.030581, 
+                            37.331800
+                        ]
+                    },
+                    "properties": {
+                        "timestamp": "2015-10-01T08:00:00-0700", 
+                        "altitude": 0,
+                        "speed": 4,
+                        "horizontal_accuracy": 30,
+                        "vertical_accuracy": -1,
+                        "motion": ["driving","stationary"],
+                        "pauses": false,
+                        "activity": "other_navigation",
+                        "desired_accuracy": 100,
+                        "deferred": 1000,
+                        "significant_change": "disabled",
+                        "locations_in_payload": 1,
+                        "battery_state": "charging",
+                        "battery_level": 0.80,
+                        "device_id": "",
+                        "wifi": ""
+                    }
+                }
+            ]
         }
-        res = make_response(jsonify(response),200)
+        res = make_response(jsonify(incoming),200)
         return res 
     else:
         res = make_response(jsonify({"message": "No JSON recd"}), 418) 
         return res 
-
+                        
